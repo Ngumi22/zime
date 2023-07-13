b033a9268d556a932ebef5cdead336e872196e59
ngumi22 <mwaurapngumi@gmail.com>
2023-07-13 09:42:23 +0300

    first

---
+++ index.js
@@ -0,0 +1,499 @@
+const categories = [
+  {
+    title: 'laptops',
+    items: [
+      {
+        id: 0,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+        title: 'First Laptop',
+        price: 100,
+        color: 'red'
+      },
+      {
+        id: 1,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+        title: 'Second Laptop',
+        price: 200,
+        color: 'green'
+      },
+      {
+        id: 2,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'Third Laptop',
+        price: 300,
+        color: 'green'
+      },
+      {
+        id: 3,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Fourth Laptop',
+        price: 400,
+        color: 'green'
+      },
+    ]
+  },
+  {
+    title: 'computers',
+    items: [
+      {
+        id: 4,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+        title: 'First Cmp',
+        price: 100,
+        color: 'red'
+      },
+      {
+        id: 5,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+        title: 'Second Comp',
+        price: 200,
+        color: 'green'
+      },
+      {
+        id: 6,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'Third Comp',
+        price: 200,
+        color: 'green'
+      },
+      {
+        id: 7,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Fourth Comp',
+        price: 200,
+        color: 'green'
+      },
+      // Add more items for the Computers category
+    ]
+  },
+  {
+    title: 'phones',
+    items: [
+      {
+        id: 30,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First Phone',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 31,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Phone',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 32,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Phone',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 33,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Phone',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+  {
+    title: 'accessories',
+    items: [
+      {
+        id: 10,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First Accessories',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 11,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Accessories',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 12,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Accessories',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 13,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Accessories',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+  {
+    title: 'printers',
+    items: [
+      {
+        id: 20,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First Printer',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 21,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Printers',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 22,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Printers',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 23,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Printers',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+  {
+    title: 'monitors',
+    items: [
+      {
+        id: 90,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First Monitors',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 91,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Monitors',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 92,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Monitors',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 93,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second Monitors',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+
+  {
+    title: 'networking',
+    items: [
+      {
+        id: 50,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First network',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 51,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second network',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 52,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Third network',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 53,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Fourth network',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 54,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Fifth network',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 55,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Sixth network',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 56,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Seventh network',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+
+  {
+    title: 'deals',
+    items: [
+      {
+        id: 70,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        title: 'First deal',
+        price: 300,
+        color: 'blue'
+      },
+      {
+        id: 71,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 72,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 73,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 74,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 75,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      {
+        id: 76,
+        image: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        title: 'Second deal',
+        price: 400,
+        color: 'yellow'
+      },
+      // Add more items for the Phones & Tablets category
+    ]
+  },
+  // Add more categories
+];
+
+
+
+// Add an event listener to each category title
+
+
+
+categories.forEach(category => {
+  const categoryLinks = document.querySelectorAll(`a[href="#${category.title}"]`);
+  categoryLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      renderCards(category);
+    });
+  });
+});
+
+function renderCards(category = null) {
+  const cardsContainer = document.getElementById('cards');
+  cardsContainer.innerHTML = '';
+
+  let activeCategory;
+  if (category) {
+    activeCategory = category;
+  } else {
+    activeCategory = categories[0];
+  }
+
+  activeCategory.items.forEach((item, index) => {
+    const { image, title, price, color } = item;
+    const card = createCardElement(image, title, price, color, index, activeCategory.title);
+    cardsContainer.appendChild(card);
+  });
+}
+
+// Rest of the code remains the same
+
+
+
+
+
+
+function createCardElement(image, title, price, color, index, categoryTitle) {
+  const card = document.createElement('div');
+  card.classList.add('product-card');
+
+  card.innerHTML = `
+    <a href="#" class="group justify-items-center">
+      <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img src="${image}" alt="Product" class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        </div>
+      <h3 class="mt-4 text-sm text-gray-700 name">${title}</h3>
+      <h3 class="mt-4 text-sm text-gray-700 color">${color}</h3>
+      <div class="d-flex justify-content-between">
+        <p class="mt-1 font-medium text-gray-900 price">Ksh ${price}.00</p>
+        <button class="mt-1 font-medium text-gray-900" onclick="addToCart(${index}, '${categoryTitle}')">
+          <i class="bi bi-cart"></i>
+        </button>
+      </div>
+    </a>
+  `;
+
+  return card;
+}
+
+function addToCart(index, categoryTitle) {
+  const selectedCategory = categories.find(category => category.title === categoryTitle);
+  const selectedItem = selectedCategory.items[index];
+  const existingCartItem = cart.find(item => item.id === selectedItem.id);
+
+  if (existingCartItem) {
+    existingCartItem.quantity++;
+    existingCartItem.totalPrice = existingCartItem.quantity * existingCartItem.price;
+  } else {
+    const newCartItem = {
+      ...selectedItem,
+      quantity: 1,
+      totalPrice: selectedItem.price
+    };
+    cart.push(newCartItem);
+  }
+
+  displayCart();
+  updateCartStorage();
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  displayCart();
+  updateCartStorage();
+}
+
+function displayCart() {
+  const cartItemContainer = document.getElementById('cart-item');
+  const countElement = document.getElementById('count');
+  const totalElement = document.getElementById('total');
+  let total = 0;
+  let count = 0;
+
+  cartItemContainer.innerHTML = '';
+
+  if (cart.length === 0) {
+    cartItemContainer.textContent = 'Your cart is empty';
+    countElement.textContent = '0';
+    totalElement.textContent = 'Ksh 0.00';
+  } else {
+    cart.forEach((item, index) => {
+      const { image, title, price, color, quantity, totalPrice } = item;
+      total += totalPrice;
+      count += quantity;
+
+      const cartItem = document.createElement('li');
+      cartItem.classList.add('flex', 'py-6');
+
+      cartItem.innerHTML = `
+        <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img src="${image}" alt="Product">
+        </div>
+
+        <div class="ml-4 flex flex-1 flex-col">
+          <div>
+            <div class="flex justify-between text-base font-medium text-gray-900">
+              <h3>
+                <a>${title}</a>
+              </h3>
+              <p class="ml-4 unit-price">Ksh ${price}.00</p>
+            </div>
+            <p class="mt-1 text-sm text-gray-500">${color}</p>
+          </div>
+          <div class="flex flex-1 items-end justify-between text-sm">
+            <p class="text-gray-500">Qty <input id="quantity-${index}" min="1" name="quantity" value="${quantity}" type="number" class="form-control form-control-sm" onchange="updateQuantity(${index}, this.value)"></p>
+            <div class="flex">
+              <button type="button" onclick="removeFromCart(${index})" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+            </div>
+          </div>
+        </div>
+      `;
+
+      cartItemContainer.appendChild(cartItem);
+    });
+
+    countElement.textContent = count.toString();
+    totalElement.textContent = `Ksh ${total.toFixed(2)}`;
+
+    const unitPriceElements = document.querySelectorAll('.unit-price');
+    unitPriceElements.forEach((element, index) => {
+      const cartItem = cart[index];
+      element.textContent = `Ksh ${cartItem.price * cartItem.quantity}.00`;
+    });
+  }
+}
+
+function updateQuantity(index, newQuantity) {
+  const cartItem = cart[index];
+  cartItem.quantity = parseInt(newQuantity);
+  cartItem.totalPrice = cartItem.quantity * cartItem.price;
+  displayCart();
+  updateCartStorage();
+}
+
+function updateCartStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem('cartCount', cart.length);
+}
+
+function loadCartFromStorage() {
+  const savedCart = localStorage.getItem('cart');
+  const savedCartCount = localStorage.getItem('cartCount');
+
+  if (savedCart) {
+    cart = JSON.parse(savedCart);
+  }
+
+  if (savedCartCount) {
+    const countElement = document.getElementById('count');
+    countElement.textContent = savedCartCount;
+  }
+}
+
+renderCards();
+loadCartFromStorage();
+displayCart();
